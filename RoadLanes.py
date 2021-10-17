f41905771b3f76068f07e1b0d84553b9e233f1dd
Sienq <hubert.sienkiewicz99@gmail.com>
2021-10-17 02:21:45 +0200

    slightly changed thresholding algorithm, added description to functions/methods and key elements of Road lanes detector

--- RoadLanes.py
+++ RoadLanes.py
@@ -6,69 +6,92 @@ import matplotlib.pyplot as plt
 
 class LaneDetector():
     def __init__(self,frame):
+
         self.frame = frame
 
-    def preprocess_frame(self,threshold1=100,threshold2=255,kernelSize = (3,3)):
-        self.grayframe = cv2.cvtColor(self.frame,cv2.COLOR_BGR2GRAY)
-        self.grayframe = cv2.addWeighted(self.grayframe,0.02,np.zeros(self.grayframe.shape,self.grayframe.dtype),20,-20)
-        cv2.imshow('gray',self.grayframe)
+    #!Convert img to HLS and get only Light channel
+    def preprocess_frame(self,threshold1=100,threshold2=255,kernelSize = (7,7)):
 
-        self.grayframe = cv2.GaussianBlur(self.frame,ksize=(3,3),sigmaX=0)
-        self.filteredFrame = cv2.Canny(self.grayframe,threshold1,threshold2,kernelSize)
+        # self.frame = cv2.GaussianBlur(self.frame,kernelSize,sigmaX=0)
+        self.hlsImg = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HLS).astype(float)     # Convert from BGR to H-HUE L-LIGHT S-Saturation format
+        self.hlsLight = self.hlsImg[:,:,1]      # Get only L channel
 
-        self.hlsImg = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HLS).astype(float)
-        self.hlsLight = self.hlsImg[:,:,1]
-
-
+    #!Create mask over ROI where road lane appears,
     def get_perspective(self,frame):
+
         mask = np.zeros_like(frame)
         h,w= mask.shape
-        center = (int(w/2),int(h/2))
+        center = (int(w/2),int(h/2))    # center of video frame
         cords = np.array([[[center[0]+150,center[1]-20],
                            [center[0]-100,center[1]-20],
                            [0+50,h-225],
-                           [w-50,h-225]]])
-        mask = cv2.fillPoly(mask,cords,(255,255,255))
-        self.maskedImg = cv2.bitwise_and(frame,mask)
-        cv2.imshow('maskedImg',self.maskedImg)
+                           [w-50,h-225]]])      # coordinates of rectangle where road lanes are
+        mask = cv2.fillPoly(mask,cords,(255,255,255)) 
+        self.maskedImg = cv2.bitwise_and(frame,mask)    # bitwise operation to make all elements except mask black
 
-        masksize = np.array([[0,0],[1024,0],[0,600],[1024,600]],np.float32)
+        masksize = np.array([[0,0],[1024,0],[0,600],[1024,600]],np.float32)     #size of mask in "bird eye" view
         cords = np.squeeze(cords).astype(np.float32)
-        cords = cords[[1,0,2,3]]
-        perspective= cv2.getPerspectiveTransform(cords,masksize)
-        self.perspectiveImg= cv2.warpPerspective(frame,perspective,(1024,600),flags=cv2.INTER_LANCZOS4)
+        cords = cords[[1,0,2,3]]    # rearrange cords for get "getperspectivetransform" function
+        perspective= cv2.getPerspectiveTransform(cords,masksize)    # get perspective from original image to "bird eye" view
+        self.perspectiveImg= cv2.warpPerspective(frame,perspective,(1024,600),flags=cv2.INTER_LANCZOS4)     # extract "bird eye" image
 
-
+    #!Prepare frame for making histogram
     def get_img_for_histogram(self):
 
-       edge = cv2.Scharr(self.perspectiveImg,cv2.CV_64F,1,0)
-       edge = np.absolute(edge)
-       edge = np.uint8(255* edge / np.max(edge))
+       edge = cv2.Scharr(self.perspectiveImg,cv2.CV_64F,1,0)    # apply Schaar filter for edge detection
+       edge = np.absolute(edge)     # we don't care if it's positive or negative
+       edge = np.uint8(255* edge / np.max(edge))    # convert to 0-255 scale
        cv2.imshow('edges',edge)
 
-       self.binaryOutputFrame = np.zeros_like(edge)
-       self.binaryOutputFrame[edge >= 50] = 255
-       cv2.imshow('binary',self.binaryOutputFrame)
+       return edge
 
-    def detect_lines(self):
-        partialFrame = self.binaryOutputFrame[self.binaryOutputFrame.shape[0] * 2 // 3:,:]
-        cv2.imshow('partial',partialFrame)
-        histogram = np.sum(partialFrame,axis=0)
+    def threshold(self):
+
+        frame = self.get_img_for_histogram() # get frame afer filtering with Scharr
+        self.binaryOutput = np.zeros_like(frame)
+
+        height = self.binaryOutput.shape[0]
+        thresholdChangeUp = 15
+        thresholdChangeDown = 60
+        thresholdChangeDelta = thresholdChangeDown-thresholdChangeUp
+        
+        for yindx in range(height):     # change threshold gradually to avoid disappearing line on top side of frame
+            binLine = self.binaryOutput[yindx,:]
+            edgeLine = frame[yindx,:]
+            thresholdLine = thresholdChangeUp + thresholdChangeDelta * yindx / height
+            binLine[edgeLine >= thresholdLine] = 255
+
+
+        self.binaryOutput[(self.perspectiveImg >=140) & (self.perspectiveImg <= 255)] = 255
+        # binaryThreshold = np.zeros_like(self.perspectiveImg)
+        # binaryThreshold[(self.perspectiveImg >=140) & (self.perspectiveImg <= 255)] = 255
+        cv2.imshow('binary',self.binaryOutput)
+
+
+    #!Create histograms from tresholded "bird eye" view
+    def create_histograms(self):
+
+        self.partialFrame = self.binaryOutput[self.binaryOutput.shape[0] * 2 // 3:,:] # get small part of whole masked area
+        cv2.imshow('partial',self.partialFrame)
+        histogram = np.sum(self.partialFrame,axis=0)    # create histogram
         # plt.plot(histogram)
         # plt.show()
 
-        leftLane = np.argmax(histogram[0:len(histogram)//2])
-        rightLane = np.argmax(histogram[len(histogram)//2:])
+        leftLane = np.argmax(histogram[0:len(histogram)//2])    # left side of histogram is for left lane
+        rightLane = np.argmax(histogram[len(histogram)//2:])    # right side of histogram is for right lane
 
-        windowHeight = self.binaryOutputFrame.shape[0]/15 #num_windows
+        return leftLane,rightLane
+
+    def detect_lines(self):
+
+        windowHeight = self.binaryOutput.shape[0]/15 #num_windows
         margin = 80
         minPixels = 50
-        nonZero = self.binaryOutputFrame.nonzero()
+        nonZero = self.binaryOutput.nonzero()
         nonZeroY = np.array(nonZero[0])
         nonzeroX = np.array(nonZero[1])
 
-        leftX = leftLane
-        rightX = rightLane
+        leftX,rigthX = self.create_histograms()
 
         leftLaneIndexes = []
         rightLaneIndexes = []
@@ -76,13 +99,13 @@ class LaneDetector():
         for idx in range(15):
             windowXleftMin = leftX - margin
             windowXleftMax = leftX + margin
-            windowXrightMin = leftX - margin
-            windowXrightMax = leftX + margin
+            windowXrightMin = rigthX - margin
+            windowXrightMax = rigthX + margin
 
-            windowYTop = partialFrame.shape[1] - idx * windowHeight
+            windowYTop = self.partialFrame.shape[1] - idx * windowHeight
             windowYBottom = windowYTop - windowHeight
 
-            cv2.imshow('lines',partialFrame)
+            cv2.imshow('lines',self.partialFrame)
 
 
 if __name__ == '__main__':
@@ -95,9 +118,9 @@ if __name__ == '__main__':
         capfordetection.preprocess_frame()
         capfordetection.get_perspective(capfordetection.hlsLight)
         capfordetection.get_img_for_histogram()
+        capfordetection.threshold()
         capfordetection.detect_lines()
         cv2.imshow('cam',capfordetection.frame)
-        cv2.imshow('canny',capfordetection.filteredFrame)
         if cv2.waitKey(30) & 0xFF == ord('q'):
             break
 
